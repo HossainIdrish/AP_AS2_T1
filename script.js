1e8a074cb9082feb4958aa7b81e754f64ac464ea
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-30 16:40:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
   const webmap = new WebMap({
     portalItem: {
-      id: "d3743be335b6443e94b4126873fa1a88"
+      id: "630750de135f4a7fbd2acfc257ae0e2f"
     }
   });
 
